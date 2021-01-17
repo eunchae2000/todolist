3addb4cbb742fa043f3f8d1127fa362b0022abf5
eunchae2000 <tldjs3651@naver.com>
2021-01-17 22:29:18 +0900

    weather.js 수정   css 까지 수정 완료 todolist 완성

--- weather.js
+++ weather.js
@@ -1,58 +1,56 @@
-
 const weather = document.querySelector(".js-weather");
-const API = "8389cde55682e09ee5d44e84bc24cf2b";
-const COORDS = "coords";
-
-function saveWeather(coordsObj){
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
-}
-
-function handleSucces(position){
-    const latitude = position.coords.latitude;
-    const longtitude = position.coords.longtitude;
-    const coords = {
-        latitude : latitude,
-        longtitude : longtitude
-    };
-    saveWeather(coords);
-    getWeather(latitude, longtitude);
-}
-
-function handleError(){
-    console.log("Can't access geo locaion.");
-}
-
-function askWeather(){
-    navigator.geolocation.getCurrentPosition(handleSucces, handleError);
-}
+const API_KEY = "1684e7a72f33d056e3bccb08769f6063";     
+const COORDS = 'coords';
 
 function getWeather(lat, lng){
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API}`
-        ).then(function(respone){
-        return respone.json();
-    })
-    .then(function(json){
-        const temperature = json.main.temp;
-        const place = json.name;
-        weather.innerText = `${temperature}ºc / ${place}`;
-    });
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+        ).then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature}ºc / ${place}`;
+        });
+    }
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-function loadWeather(){
-    const loadcoords = localStorage.getItem(COORDS);
-    if(loadcoords == null){
-        askWeather();
-    }
-    else{
-        const parsedWeather = JSON.stringify(loadcoords);
-        getWeather(parsedWeather.latitude, parsedWeather.longtitude);
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;     
+    const longitude = position.coords.longitude;   
+    const coords = {
+        latitude: latitude,
+        longitude: longitude
+    };
+    saveCoords(coords);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+    console.log("Can't access geo location");
+}
+
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+   
+function loadCords(){
+    const loadCords = localStorage.getItem(COORDS);
+    if(loadCords === null){
+        askForCoords();
+    }else{
+        const parseCoords = JSON.parse(loadCords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
 function init(){
-    loadWeather();
+    loadCords();
 }
 
 init();
-
